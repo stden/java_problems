693d6d5d6e57c08dd3aa0daf5e2a9442ffc796bf
Denis Stepulenok <super.denis@gmail.com>
2016-11-23 12:12:23 +0300

    Заданная сумма x из 3-х массивов: a,b,c

--- src/test/java/interview/Sums3Test.java
+++ src/test/java/interview/Sums3Test.java
@@ -3,7 +3,13 @@ package interview;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class Sums3Test extends Assert {
+
+    private static boolean DEBUG = false;
+    private Random gen = new Random();
 
     public static void main(String[] args) {
         // Даны три массива
@@ -15,11 +21,29 @@ public class Sums3Test extends Assert {
         int x = 6;
 
         System.out.println("calc(a,b,c,x) = " + calc(a, b, c, x));
-
     }
 
+    /**
+     * Заданная сумма x из 3-х массивов: a,b,c.
+     * Первое слагаемое из a, второе - из b, третье из c
+     *
+     * @param a первый массив
+     * @param b второй массив
+     * @param c третий массив
+     * @param x заданная сумма
+     * @return Количество вариантов получить x
+     */
     private static int calc(int[] a, int[] b, int[] c, int x) {
         int count = 0; // Количество способов
+        // Сортируем массивы
+        Arrays.sort(b);
+        Arrays.sort(c);
+        if (DEBUG) {
+            System.out.println(Arrays.toString(a));
+            System.out.println(Arrays.toString(b));
+            System.out.println(Arrays.toString(c));
+            System.out.println("x = " + x);
+        }
         for (int ax : a) {
             int sum = x - ax; // Оставшаяся сумма
             int j = 0; // Указатель в массиве b
@@ -40,12 +64,13 @@ public class Sums3Test extends Assert {
                     }
                     count += bn * cn;
                 } else if (s > sum) {
-
+                    k--;
                 } else if (s < sum) {
-
+                    j++;
                 }
             }
         }
+        if (DEBUG) System.out.println("count = " + count);
         return count;
     }
 
@@ -59,5 +84,40 @@ public class Sums3Test extends Assert {
         // Сколькими способами можно получить заданную сумму
         assertEquals("6 можно получить взяв любую из 2-х 2-ек и любую из 3-х 3-ек",
                 2 * 3, calc(a, b, c, 6));
+        assertEquals("Сравниваем прямое решение с нашим",
+                bruteForce(a, b, c, 6), calc(a, b, c, 6));
+    }
+
+    @Test
+    public void testRandom() {
+        for (int test = 0; test < 30; test++) {
+            // 3 случайных массива
+            int[] a = genArray(3 + gen.nextInt(8));
+            int[] b = genArray(3 + gen.nextInt(8));
+            int[] c = genArray(3 + gen.nextInt(8));
+            int x = a[gen.nextInt(a.length)] + b[gen.nextInt(b.length)] + c[gen.nextInt(c.length)];
+
+            // a.clone() чтобы не менять исходные массивы
+            assertEquals(bruteForce(a, b, c, x), calc(a.clone(), b.clone(), c.clone(), x));
+        }
+    }
+
+    private int bruteForce(int[] a, int[] b, int[] c, int x) {
+        int count = 0;
+        for (int ax : a)
+            for (int bx : b)
+                for (int cx : c)
+                    if (ax + bx + cx == x) {
+                        count++;
+                    }
+        return count;
+    }
+
+    private int[] genArray(int size) {
+        int[] a = new int[size];
+        for (int i = 0; i < a.length; i++) {
+            a[i] = gen.nextInt(20);
+        }
+        return a;
     }
 }
